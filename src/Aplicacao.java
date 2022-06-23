f1f16c8bcc09a9fc9b872704d10554d8d51c05f7
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-06-23 17:49:30 -0300

    Implementando pratoFeitoFactory

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,5 +1,11 @@
+import Bebidas.Bebida;
+import Bebidas.BebidaFactoryConcreto;
+import Bebidas.TipoBebida;
+
 public class Aplicacao {
     public static void main(String[] args) {
-        System.out.println("teste");
+        BebidaFactoryConcreto bebidaFactory = new BebidaFactoryConcreto();
+        Bebida agua = bebidaFactory.criarBebida(TipoBebida.AGUA);
+        Bebida cerveja = bebidaFactory.criarBebida(TipoBebida.CERVEJA);
     }
 }
